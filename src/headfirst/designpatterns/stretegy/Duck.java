26f71a0dfd13c4d0b43d312e96382cafe0293493
HoSeung <meteorproject529@gamil.com>
2023-03-29 12:08:09 +0900

    헤드 퍼스트 디자인 패턴 시작

---
+++ src/headfirst/designpatterns/stretegy/Duck.java
@@ -0,0 +1,24 @@
+package headfirst.designpatterns.stretegy;
+
+public abstract class Duck {
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    
+    public Duck() {}
+    
+    public abstract void display();
+    
+    public void performFly() {
+        flyBehavior.fly();
+    }
+    
+    public void pserformQuack() {
+        quackBehavior.quack();
+    }
+    
+    public void swim() {
+        System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠");
+    }
+    
+}
---
+++ src/headfirst/designpatterns/stretegy/FlyBehavior.java
@@ -0,0 +1,4 @@
+package headfirst.designpatterns.stretegy;
+
+public interface FlyBehavior {
+}
