ee75942bd92d55baba65a1ac0099b12c0f7836d6
HoSeung <meteorproject529@gamil.com>
2023-03-29 17:37:21 +0900

    Study: ch02. 옵저버 패턴

---
+++ src/headfirst/designpatterns/observer/swing/SwingObserverExample.java
@@ -0,0 +1,52 @@
+package headfirst.designpatterns.observer.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    private void go() {
+        frame = new JFrame();
+
+        JButton button = new JButton("할까? 말까?");
+
+
+//        button.addActionListener(new AngelListener());
+//        button.addActionListener(new DevilListener());
+
+        button.addActionListener(event -> System.out.println("하지마! 후회할껄?"));
+        button.addActionListener(event -> System.out.println("그냥 저질러!"));
+
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+
+    }
+
+    class AngelListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("하지마! 후회할껄?");
+        }
+    }
+
+
+    private class DevilListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("그냥 저질러!");
+        }
+    }
+}
