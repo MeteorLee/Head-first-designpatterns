03560e4a65a85d46eee51a70bc90403726e0a95e
MeteorLee <meteorproject529@gmail.com>
2023-07-26 15:33:50 +0900

    feat: 카페 메뉴 추가

--- src/headfirst/designpatterns/반복자_패턴과_컴포지트_패턴/Waitress.java
+++ src/headfirst/designpatterns/반복자_패턴과_컴포지트_패턴/Waitress.java
@@ -5,27 +5,33 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
 
         System.out.println("메뉴\n----\n아침 메뉴");
         printMenu(pancakeIterator);
         System.out.println("\n 점심 메뉴");
         printMenu(dinerIterator);
+        System.out.println("\n 저녁 메뉴");
+        printMenu(cafeIterator);
+
     }
 
     private void printMenu(Iterator<MenuItem> iterator) {
         while (iterator.hasNext()) {
             MenuItem menuItem = iterator.next();
-            System.out.println(menuItem.getName() + ", ");
-            System.out.println(menuItem.getPrice() + " -- ");
+            System.out.print(menuItem.getName() + ", ");
+            System.out.print(menuItem.getPrice() + " -- ");
             System.out.println(menuItem.getDescription());
         }
 
