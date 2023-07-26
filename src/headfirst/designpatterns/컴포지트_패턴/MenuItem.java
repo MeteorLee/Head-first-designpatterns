807d3a1770e8ecf99ab75d9e79deb70e3eaf4d10
MeteorLee <meteorproject529@gmail.com>
2023-07-26 19:31:55 +0900

    study: 컴포지트 패턴 학습

---
+++ src/headfirst/designpatterns/컴포지트_패턴/MenuItem.java
@@ -0,0 +1,44 @@
+package headfirst.designpatterns.컴포지트_패턴;
+
+public class MenuItem extends MenuComponent {
+    String name;
+    String description;
+    boolean vegetarian;
+    double price;
+
+    public MenuItem(String name, String description, boolean vegetarian, double price) {
+        this.name = name;
+        this.description = description;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("  " + getName());
+        if (isVegetarian()) {
+            System.out.print("(v)");
+        }
+        System.out.println(", " + getPrice());
+        System.out.println("     -- " + getDescription());
+    }
+}
---
+++ src/headfirst/designpatterns/컴포지트_패턴/Waitress.java
@@ -0,0 +1,13 @@
+package headfirst.designpatterns.컴포지트_패턴;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
