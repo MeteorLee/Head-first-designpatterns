43c1b6907c42c912e8fc61acc058a31e0e95708d
MeteorLee <meteorproject529@gmail.com>
2023-07-26 17:34:51 +0900

    chore: 패키지 수정

--- src/headfirst/designpatterns/반복자_패턴/MenuItem.java
+++ src/headfirst/designpatterns/반복자_패턴/MenuItem.java
@@ -1,4 +1,4 @@
-package headfirst.designpatterns.반복자_패턴과_컴포지트_패턴;
+package headfirst.designpatterns.반복자_패턴;
 
 public class MenuItem {
     private String name;
--- src/headfirst/designpatterns/반복자_패턴/MenuTestDrive.java
+++ src/headfirst/designpatterns/반복자_패턴/MenuTestDrive.java
@@ -1,4 +1,4 @@
-package headfirst.designpatterns.반복자_패턴과_컴포지트_패턴;
+package headfirst.designpatterns.반복자_패턴;
 
 public class MenuTestDrive {
     public static void main(String[] args) {
